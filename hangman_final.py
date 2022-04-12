86be82899e8ea351da7d5c5dbca5335c35f92fdd
Alex <98416718+alexandru-ghibea@users.noreply.github.com>
2022-04-12 16:32:12 +0300

    Update hangman_final.py

--- hangman_final.py
+++ hangman_final.py
@@ -2,34 +2,56 @@ import random
 from hangman_word import word_list
 from hangman_art import logo, stages
 
+
 chosen_word = random.choice(word_list)
 display = []
-lives = 6
-game_on = True
+lives = len(chosen_word) - 1
+#game_on = True
+
+
+def replay():
+    answer = ["Y", "N"]
+    choice = input("Do you want to play again Y or N: ")
+    while choice not in answer:
+        choice = input("Sorry, please select  Y or N: ")
+    if choice == "Y":
+        return True
+    else:
+        return False
+
 
 for letter in chosen_word:
     display.append("_")
-    
-print(logo)  
+
+print(logo)
 print(f"You have {lives} lives. Then...dead!")
 
-while game_on:
-    index = 0
-    guess = input("Guess a letter: ").lower()
-    if guess in display:
-        print(f"You've already guessed {guess}")
-    for position in chosen_word:
-        if guess == position:
-            display[index] = position
-        index += 1
-    print(" ".join(display))
-    if guess not in chosen_word:
-        lives -= 1
-        print(f"Wrong guess. Number of lives remaining {lives}")
-    if lives == 0:
+while True:
+    game_on = input("Ready to play Y or N: ").lower()
+    if game_on == "y":
+        game_on = True
+    else:
         game_on = False
-        print("You loose.Word was:", chosen_word)
-    if "".join(display) == chosen_word:
-        print("You won. Word was:", chosen_word)
-        game_on = False
-    print(stages[lives])
+
+    while game_on:
+        index = 0
+        guess = input("Guess a letter: ").lower()
+        if guess in display:
+            print(f"You've already guessed {guess}")
+        for position in chosen_word:
+            if guess == position:
+                display[index] = position
+            index += 1
+        print(" ".join(display))
+        if guess not in chosen_word:
+            lives -= 1
+            print(f"Wrong guess. Number of lives remaining {lives}")
+        if lives == 0:
+            game_on = False
+            print("You loose.Word was:", chosen_word)
+        if "".join(display) == chosen_word:
+            print("You won. Word was:", chosen_word)
+            game_on = False
+        print(stages[lives])
+    if not replay():
+        break
